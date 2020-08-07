6f2320e2d3d8e7c3ab8aa1be41c60ec3fb1639db
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-08-07 17:14:29 +0200

    todo project started

---
+++ src/todo.java
@@ -0,0 +1,15 @@
+public class todo {
+  public static void main(String[] args) {
+
+    if (args.length == 0){
+      System.out.println("Command Line Todo application\n" +
+          "=============================\n" +
+          "\n" +
+          "Command line arguments:\n" +
+          "    -l   Lists all the tasks\n" +
+          "    -a   Adds a new task\n" +
+          "    -r   Removes an task\n" +
+          "    -c   Completes an task");
+    }
+  }
+}
